ef78ffcceb5b13fd7ec20327ddfd0bc8bfea7dbb
Ron Lawson <ronlawson@gmail.com>
2019-04-11 16:11:42 -0400

    Refactor ES6 functions

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const notes = require('./notes')
+const notes = require('./notes.js')
 
 // Customize yargs version
-yargs.version('0.0.1')
+yargs.version('1.1.0')
 
-// Add command
+// Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
@@ -21,35 +21,42 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
 
-// Remove command
+// Create remove command
 yargs.command({
     command: 'remove',
-    describe: 'Remove a new note',
-    handler: function () {
-        console.log('Removing a new note!')
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
     }
 })
 
-// Read command
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
-        console.log('Reading a new note!')
-    }
-})
-
-// List command
-yargs.command({
-    command: 'list',
-    describe: 'List all notes',
-    handler: function () {
-        console.log('Listing all notes')
+    handler() {
+        console.log('Reading a note')
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,34 +1,44 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
   return 'Your notes...'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes()
-  const duplicateNotes = notes.filter(function (note) {
-    return note.title === title
-  })
+  const duplicateNotes = notes.filter((note) => note.title === title)
 
   if (duplicateNotes.length === 0) {
     notes.push({
       title: title,
       body: body
     })
-
     saveNotes(notes)
-    console.log('New note added!')
+    console.log(chalk.green.inverse('New note added!'))
   } else {
-    console.log('Note title taken!')
+    console.log(chalk.red.inverse('Note title taken!'))
   }
 }
 
-const saveNotes = function (notes) {
+const removeNote = (title) => {
+  const notes = loadNotes()
+  const notesToKeep = notes.filter((note) => note.title !== title)
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'))
+    saveNotes(notesToKeep)
+  } else {
+    console.log(chalk.red.inverse('No note found!'))
+  }
+}
+
+const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes)
   fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync('notes.json')
     const dataJSON = dataBuffer.toString()
@@ -40,5 +50,6 @@ const loadNotes = function () {
 
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 }
